8e816f037e5d775e340672758ae0cc113bc7a2d2
shuheko <shuheko181@gmail.com>
2021-02-01 22:11:04 +0900

    体重を保存

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -2,4 +2,18 @@ class WeightsController < ApplicationController
   def index
     @weights = Weight.all
   end
+
+  def new
+    @weight = Weight.new
+  end
+
+  def create
+    Weight.create(weight_params)
+  end
+
+  private
+
+  def weight_params
+    params.require(:weight).permit(:value, :date)
+  end
 end
